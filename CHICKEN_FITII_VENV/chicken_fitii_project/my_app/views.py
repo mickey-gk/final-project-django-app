a348af27b6c5061abfe3ac3619552cf2ecb2507c
mickey_gk <mickeygkatana@gmail.com>
2024-10-09 14:53:46 +0300

    update 2 -djamgo final project

--- CHICKEN_FITII_VENV/chicken_fitii_project/my_app/views.py
+++ CHICKEN_FITII_VENV/chicken_fitii_project/my_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone #for rendering data according to time and date
 
 # Create your views here.
 def index(request):
-    context = {'name': 'mickey'}
+    context = {
+        'name': 'mickey',
+        'age': '22'
+        }
     return render(request, 'index.html', context)
+
