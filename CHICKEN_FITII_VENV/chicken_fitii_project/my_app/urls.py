31e2e64fcdc77112da53e134d616e1b02b69a57a
mickey_gk <mickeygkatana@gmail.com>
2024-10-10 19:22:09 +0300

    update 3

--- CHICKEN_FITII_VENV/chicken_fitii_project/my_app/urls.py
+++ CHICKEN_FITII_VENV/chicken_fitii_project/my_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('about', views.about, name='about'),
+    path('products', views.products, name='products')
 ]
--- CHICKEN_FITII_VENV/chicken_fitii_project/my_app/views.py
+++ CHICKEN_FITII_VENV/chicken_fitii_project/my_app/views.py
@@ -1,11 +1,46 @@
 from django.shortcuts import render
-from django.utils import timezone #for rendering data according to time and date
 
-# Create your views here.
+# navigation bar
+def navigation_items():
+    return [
+        {'name':'Home', 'url':'index'},
+        {'name':'products', 'url': 'products'},
+        {'name':'Log in', 'url':'login'},
+        {'name':'Sign in', 'url':'register'},
+        {'name':'About', 'url':'about'},
+    ]
+
+def image_links():
+    return [
+        { 'url': 'images/chicken_01.png'},
+        {'url': 'images/chicken_02.png'},
+        {'url':'images/chicken_03.png'},
+        {'url':'images/chicken_04.png'}
+    ]
+
+# Creating views here.
 def index(request):
-    context = {
-        'name': 'mickey',
-        'age': '22'
-        }
-    return render(request, 'index.html', context)
+    nav_items = navigation_items()
+    images_links = image_links()  # Get the image links
+    return render(request, 'index.html', {
+        'nav_items': nav_items,
+        'images_links': images_links,  # Pass the images to the template
+    })
+
+def login(request):
+    nav_items = navigation_items()
+    return render(request, 'login.html', {'nav_items':nav_items})
+
+def register(request):
+    nav_items = navigation_items()
+    return render(request, 'register.html', {'nav_items':nav_items})
+
+def about(request):
+    nav_items = navigation_items()
+    return render(request, 'about.html', {'nav_items':nav_items})
+
+def products(request):
+    nav_items = navigation_items()
+    return render(request, 'products.html', {'nav_items': nav_items})
+
 
